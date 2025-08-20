1d16ac01189ae7d468d963fc7c3f6fe30f3d5c60
Abhinav Choubey <98657817+abhinavchoubey2000@users.noreply.github.com>
2025-08-20 20:30:13 +0000

    implemented custom react

--- 01_basic/src/main.jsx
+++ 01_basic/src/main.jsx
@@ -1,9 +1,14 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+
+const ManualComponent =function(){
+  return React.createElement('a',{href:"https:google.com",target:'_blank'}, "Click to visit google")
+} 
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  
+    <ManualComponent/>
+  
 )
